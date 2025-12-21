46c5fc92e2733be279782c3fa23bd95768ccb170
444leonn <acostaleon25@gmail.com>
2025-12-21 11:25:16 -0300

    modif: routes title & search route with name

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,11 +1,18 @@
 import { Routes } from '@angular/router';
 import { Home } from './pages/home/home';
 import { NotFound } from './pages/not-found/not-found';
+import { Search } from './pages/search/search';
 
 export const routes: Routes = [
     {
         path: '',
         component: Home,
+        title: 'Pokedex - Home',
+    },
+    {
+        path: 'search/:name',
+        component: Search,
+        title: 'Pokedex - Search',
     },
     {
         path: '**',
