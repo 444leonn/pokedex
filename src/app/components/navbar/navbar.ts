afcc90411ab37fc77a8a2ea157f1c490df37df53
444leonn <acostaleon25@gmail.com>
2025-12-21 14:15:57 -0300

    feat: dropdown menus at navbar

--- src/app/components/navbar/navbar.ts
+++ src/app/components/navbar/navbar.ts
@@ -1,11 +1,32 @@
 import { Component } from '@angular/core';
+import { FormGroup, FormControl, ReactiveFormsModule } from '@angular/forms';
+import { Router } from '@angular/router';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-navbar',
-  imports: [],
+  imports: [ReactiveFormsModule, CommonModule],
   templateUrl: './navbar.html',
   styleUrl: './navbar.css',
 })
 export class Navbar {
+  searchForm = new FormGroup({
+    searchInput: new FormControl(''),
+  })
+  isDropdownOpen = false;
+  isMobileMenuOpen = false;
 
+  constructor(private router: Router) { }
+  
+  onSubmit() {
+    this.router.navigateByUrl(`/search/${this.searchForm.value.searchInput}`)
+  }
+
+  toggleDropdown() {
+    this.isDropdownOpen = !this.isDropdownOpen;
+  }
+
+  toggleMobileMenu() {
+    this.isMobileMenuOpen = !this.isMobileMenuOpen;
+  }
 }
