ba9c93603870812bd67b15ecb2ce24464b883b7d
444leonn <acostaleon25@gmail.com>
2025-12-21 15:41:52 -0300

    modif: not found page now uses the notfound component

--- src/app/pages/not-found/not-found.ts
+++ src/app/pages/not-found/not-found.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { Navbar } from '../../components/navbar/navbar';
 import { Footer } from '../../components/footer/footer';
+import { NotFoundComp } from '../../components/not-found-comp/not-found-comp';
 
 @Component({
   selector: 'not-found',
-  imports: [Navbar, Footer],
+  imports: [Navbar, Footer, NotFoundComp],
   templateUrl: './not-found.html',
   styleUrl: './not-found.css',
 })
