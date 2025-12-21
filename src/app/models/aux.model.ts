1720196daa0fdca21196a41ecef23b43d15b47e1
444leonn <acostaleon25@gmail.com>
2025-12-21 16:37:21 -0300

    modif: added shiny back sprite to model

--- src/app/models/aux.model.ts
+++ src/app/models/aux.model.ts
@@ -2,6 +2,7 @@ export interface Sprites {
   front_default: string | null;
   back_default: string | null;
   front_shiny: string | null;
+  back_shiny: string | null;
 
   other?: {
     'official-artwork'?: {
