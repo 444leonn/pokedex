24d68157e2ba95c3cb795c4718ce28d284f0d346
444leonn <acostaleon25@gmail.com>
2025-12-20 22:08:56 -0300

    fix: no se renderizaba la tarjeta

--- src/app/components/pokemon-card/pokemon-card.ts
+++ src/app/components/pokemon-card/pokemon-card.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input, ChangeDetectorRef } from '@angular/core';
 import { Pokemon } from '../../models/pokemon.model';
 import { HttpClient } from '@angular/common/http';
 
@@ -9,16 +9,30 @@ import { HttpClient } from '@angular/common/http';
   styleUrl: './pokemon-card.css',
 })
 export class PokemonCard implements OnInit {
-  name?: string | null;
-  pokemon?: Pokemon;
-  
-  constructor(private http: HttpClient) {}
+  @Input() name?: string | null;
+  pokemon?: Pokemon | undefined;
+  loading: boolean = false;
+  error: boolean = false;
+
+  constructor(private http: HttpClient, private cdr: ChangeDetectorRef) {}
 
   ngOnInit(): void {
     if (this.name) {
+      this.loading = true;
       this.http.get<Pokemon>(`https://pokeapi.co/api/v2/pokemon/${this.name}`)
-      .subscribe(data => {
-        this.pokemon = data;
+      .subscribe({
+        next: (response) => {
+          console.log(this.name);
+          console.log(response);
+          this.pokemon = response;
+          this.loading = false;
+          this.cdr.detectChanges();
+        },
+        error: (err) => {
+          this.error = true;
+          this.loading = false;
+          this.cdr.detectChanges();
+        }
       });
     }
   }
