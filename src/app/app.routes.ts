a9ad42bcca5828cd584b156d3690e2307af42f4d
444leonn <acostaleon25@gmail.com>
2025-12-21 14:35:30 -0300

    feat: types page

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,6 +2,7 @@ import { Routes } from '@angular/router';
 import { Home } from './pages/home/home';
 import { NotFound } from './pages/not-found/not-found';
 import { Search } from './pages/search/search';
+import { TypesPage } from './pages/types-page/types-page';
 
 export const routes: Routes = [
     {
@@ -13,6 +14,11 @@ export const routes: Routes = [
         path: 'search/:name',
         component: Search,
         title: 'Pokedex - Search',
+    },
+    {
+        path: 'types/:name',
+        component: TypesPage,
+        title: 'Pokedex - Types',
     },
     {
         path: '**',
---
+++ src/app/pages/types-page/types-page.ts
@@ -0,0 +1,46 @@
+import { Component, ChangeDetectorRef, OnInit } from '@angular/core';
+import { ActivatedRoute, ParamMap } from '@angular/router';
+import { HttpClient,httpResource } from '@angular/common/http';
+import { PokemonTypeData } from '../../models/aux.model';
+import { Navbar } from "../../components/navbar/navbar";
+import { Footer } from "../../components/footer/footer";
+import { PokemonCardSearch } from "../../components/pokemon-card-search/pokemon-card-search";
+
+
+@Component({
+  selector: 'app-types-page',
+  imports: [Navbar, Footer, PokemonCardSearch],
+  templateUrl: './types-page.html',
+  styleUrl: './types-page.css',
+})
+export class TypesPage implements OnInit {
+  typeData?: PokemonTypeData | undefined;
+  typeName?: string | null;
+  loading: boolean = false;
+  error: boolean = false;
+
+  constructor(private http: HttpClient, private cdr: ChangeDetectorRef, private route: ActivatedRoute) {}
+
+  ngOnInit() {
+    this.route.paramMap.subscribe((params: ParamMap) => {
+      this.typeName = params.get('name') || '';
+    });
+
+    if (this.typeName) {
+      this.loading = true;
+      this.http.get<PokemonTypeData>(`https://pokeapi.co/api/v2/type/${this.typeName}`)
+        .subscribe({
+          next: (response) => {
+            this.typeData = response;
+            this.loading = false;
+            this.cdr.detectChanges();
+          },
+          error: (err) => {
+            this.error = true;
+            this.loading = false;
+            this.cdr.detectChanges();
+          }
+        });
+    }
+  }
+}
